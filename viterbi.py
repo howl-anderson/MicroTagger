10db96e5a4733abcb48eb68cb09dbb4938334f87
Xiaoquan Kong <u1mail2me@gmail.com>
2018-06-29 14:45:35 +0800

    viterbi algorithms is implemented

--- viterbi.py
+++ viterbi.py
@@ -1,3 +1,7 @@
+import operator
+import functools
+
+
 class Viterbi(object):
     def __init__(self, A, B, start_state='<start>', end_state='<end>'):
         self.A = A
@@ -10,6 +14,7 @@ class Viterbi(object):
 
     def _do_predict(self, word_list):
         N = len(word_list)
+        T = N - 1
         trackback = {}
 
         all_states = self.A.keys() - {self.start_state}  # remove start_state
@@ -19,7 +24,7 @@ class Viterbi(object):
             self.trellis[state] = {}
             self.trellis[state][0] = self.A[self.start_state].get(state, 0) * self.B[state].get(word_list[0], 0)
             trackback[state] = {}
-            trackback[state][0] = 0
+            trackback[state][0] = self.start_state
 
         # recursion step
         for step in range(1, N):
@@ -29,10 +34,15 @@ class Viterbi(object):
                 # compute all previous path
                 candidate_list = []
                 for i in all_states:
-                    candidate = self.trellis[i][step - 1] * self.A[i].get(state, 0) * self.B[state].get(word, 0)
+                    previous_path_probability = self.trellis[i][step - 1]
+                    transition_probability = self.A[i].get(state, 0)
+                    state_observation_likelihood = self.B[state].get(word, 0)
+
+                    candidate = previous_path_probability * transition_probability * state_observation_likelihood
                     candidate_list.append([i, candidate])
 
-                sorted_candidate_list = sorted(candidate_list, key=lambda x: x[1])
+                sorted_candidate_list = sorted(candidate_list, key=lambda x: x[1],
+                                               reverse=True)  # NOTE: descending sort by reverse = True
                 self.trellis[state][step] = sorted_candidate_list[0][1]
 
                 trackback[state][step] = sorted_candidate_list[0][0]
@@ -43,7 +53,8 @@ class Viterbi(object):
             candidate = self.trellis[i][N-1] * self.A[i].get(self.end_state, 0)
             candidate_list.append([i, candidate])
 
-        sorted_candidate_list = sorted(candidate_list, key=lambda x: x[1])
+        sorted_candidate_list = sorted(candidate_list, key=lambda x: x[1],
+                                       reverse=True)  # NOTE: descending sort by reverse = True)
 
         self.trellis[self.end_state] = {}
         self.trellis[self.end_state][N-1] = sorted_candidate_list[0][1]
@@ -51,7 +62,31 @@ class Viterbi(object):
         trackback[self.end_state] = {}
         trackback[self.end_state][N-1] = sorted_candidate_list[0][0]
 
-        pass
+        return trackback
+
+    def predict_state(self, word_list):
+        traceback = self._do_predict(word_list)
+
+        N = len(word_list)
+        T = N - 1
+
+        reverse_state_sequence = []
+
+        reverse_state_sequence.append(self.end_state)
+
+        state = traceback[self.end_state][T]
+        reverse_state_sequence.append(state)
+
+        previous_state = state
+
+        reverse_step = reversed(range(len(word_list)))
+        for step in reverse_step:
+            state = traceback[previous_state][step]
+            reverse_state_sequence.append(state)
+
+            previous_state = state
+
+        return list(reversed(reverse_state_sequence))
 
 
 if __name__ == "__main__":
@@ -59,3 +94,6 @@ if __name__ == "__main__":
     B = {'C': {'人': 0.5, '中国人': 0.5}, 'A': {'你': 0.5, '我': 0.5}, 'B': {'是': 0.5, '打': 0.5}}
     viterbi = Viterbi(A, B)
     viterbi._do_predict(["我", "是", "中国人"])
+
+    state_sequence = viterbi.predict_state(["我", "是", "中国人"])
+    print(state_sequence)
